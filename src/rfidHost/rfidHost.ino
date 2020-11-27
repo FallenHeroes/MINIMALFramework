81fc384f0d1c24e2f139c7435858267e632572b5
FallenHeroes <108849830+FallenHeroes@users.noreply.github.com>
2020-11-27 09:46:58 +0000

    Edit Rulespage

--- src/rfidHost/rfidHost.ino
+++ src/rfidHost/rfidHost.ino
@@ -12,4 +12,5 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
-  Serial.println("Approximate your card to the reader...")
+  Serial.println("Approximate your card to the reader...");
+  Se
