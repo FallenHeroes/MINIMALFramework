9f27e4381de440ca68ba1b6cc8eac489e6e613ce
FallenHeroes <108849830+FallenHeroes@users.noreply.github.com>
2021-05-12 19:59:59 +0000

    update coding style

--- src/rfidHost/rfidHost.ino
+++ src/rfidHost/rfidHost.ino
@@ -51,4 +51,4 @@ String readMacAddressFromTag()
 
   MFRC522::StatusCode status;
   
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(
