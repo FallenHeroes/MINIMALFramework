9899344e7f6d720eabaf81d37868d5d66191e12f
FallenHeroes <108849830+FallenHeroes@users.noreply.github.com>
2021-07-15 10:43:30 +0000

    Add more cave details

--- src/rfidHost/rfidHost.ino
+++ src/rfidHost/rfidHost.ino
@@ -58,4 +58,4 @@ String readMacAddressFromTag()
     return String("PCD_Authenticate() failed");
   }
 
-  status = mfrc522.MIFARE_Read(block, conte
+  status = mfrc522.MIFARE_Read(block, content, &
