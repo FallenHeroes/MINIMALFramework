f0b09eeff65dc9d21630c264523c3fcfc3e94e80
FallenHeroes <108849830+FallenHeroes@users.noreply.github.com>
2022-10-15 07:38:58 +0000

    Create test.py

--- src/rfidHost/rfidHost.ino
+++ src/rfidHost/rfidHost.ino
@@ -109,4 +109,5 @@ void writeMacAddressIntoTag(String macAddress)
 
 
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
-  if (status != MFRC522::STATUS_OK)
+  if (status != MFRC522::STATUS_OK) {
+  
