d9fc168d72429503f12adc51b8072ba4ddd8fafb
FallenHeroes <108849830+FallenHeroes@users.noreply.github.com>
2023-01-30 17:26:27 +0000

    merged w/ master

--- src/rfidHost/rfidHost.ino
+++ src/rfidHost/rfidHost.ino
@@ -117,4 +117,4 @@ void writeMacAddressIntoTag(String macAddress)
   else Serial.println(F("PCD_Authenticate() success: "));
 
   // Write block
-  status = mfrc522.MIFARE_Write(block, b
+  status = mfrc522.MIFARE_Write(block, buffe
