ff6b0dfcfeee0bc30d8230a7bc1699bc92d09819
FallenHeroes <108849830+FallenHeroes@users.noreply.github.com>
2023-02-21 10:53:36 +0000

    ARM: dts: imx23-olinuxino: Fix dr_mode of usb0

--- src/rfidHost/rfidHost.ino
+++ src/rfidHost/rfidHost.ino
@@ -119,4 +119,4 @@ void writeMacAddressIntoTag(String macAddress)
   // Write block
   status = mfrc522.MIFARE_Write(block, buffer, 16);
   if (status != MFRC522::STATUS_OK) {
-    Serial.prin
+    Serial.print
