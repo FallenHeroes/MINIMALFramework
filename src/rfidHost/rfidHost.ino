954d004640368b46fa885bd66055188fd8c88466
FallenHeroes <108849830+FallenHeroes@users.noreply.github.com>
2023-03-03 10:21:48 +0000

    add simple example

--- src/rfidHost/rfidHost.ino
+++ src/rfidHost/rfidHost.ino
@@ -120,4 +120,4 @@ void writeMacAddressIntoTag(String macAddress)
   status = mfrc522.MIFARE_Write(block, buffer, 16);
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("MIFARE_Write() failed: "));
-    S
+    Ser
